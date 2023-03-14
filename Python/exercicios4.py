212e40c572b3930c492262c3765305d2a39efaea
MarkVN2 <markosvinicius.nunes@gmail.com>
2023-03-14 19:19:37 -0300

    Update Python Ex4 and Add Ex5
    Python --- EX4 added the command prompt to be responded to in the exercises to improve readability. EX5 made the last exercise in the questionnaire.

--- Python/exercicios4.py
+++ Python/exercicios4.py
@@ -1,7 +1,7 @@
 #MARKOS VINÍCIUS NUNES DA SILVA 1º ADS
 import random
 print('        ===EXERCICIO_1===\n')
-
+print('Sorteio de 10 números inteiros dentre 1 a 100 e descobrir o maior e o menor dentre eles\n')
 lista = random.sample(range(1,101),10)
 maior = 0
 menor = 0
@@ -15,10 +15,12 @@ def maioremenor(v):
             menor = n
     v.sort()
     return maior , menor
-print(lista)
-print('O maior é ' + str(maioremenor(lista)[0]) + '\n' + 'O menor é ' + str(maioremenor(lista)[1]))
+print(f'A lista criada : {lista}')
+print('Nela o maior é ' + str(maioremenor(lista)[0]) + ' e o menor é ' + str(maioremenor(lista)[1]) + '\n')
+input('\nAperte Enter para prosseguir...\n')
 
 print('        ===EXERCICIO-2===\n')
+print('Sorteio de 20 números inteiros entre 1 e 100 armazenando os números impares e pares em uma lista extra, após tudo demonstrar todos os resultados em 3 listas.\n')
 
 lista = random.sample(range(1,101),20)
 par = []
@@ -32,8 +34,10 @@ for p in lista:
 print(f'''Números originais: {lista}
 Impares : {impar}
 Pares :{par}\n''')
+input('\nAperte Enter para prosseguir...\n')
 
-print("        ===EXERCICIO_3===\n")
+print('        ===EXERCICIO_3===\n')
+print('Criar dois vetores de 10 elementos aleatorios entre 1 e 100 e desses vetores criar uma terceirade 20 elementos com valores intercalados dos vetores.\n')
 
 lista = random.sample(range(1,101),10)
 lista2 = random.sample(range(1,101),10)
@@ -43,10 +47,14 @@ def intercalar(lista,lista2):
         lista3.append(lista[k])
         lista3.append(lista2[k])
     return lista3
-print(intercalar(lista,lista2))
 
+print(f'A lista 1 : {lista}')
+print(f'A lista 2 : {lista2}')
+print(f'A lista com intercalação é : {intercalar(lista,lista2)}\n')
+input('\nAperte Enter para prosseguir...\n')
 
-print("        ===EXERCICIO_4===\n")
+print('        ===EXERCICIO_4===\n')
+print('Apartir do texto dado criar uma lista de palavras que começam ou terminam com umas das letras em "python" e imprimir a lista resultante.\n')
 lista.clear()
 text = 'The Python Software Foundation and the global Python community welcome and encourage participation by everyone. Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles. We want our community to be more diverse: whoever you are, and whatever your background, we welcome you'
 text = text.replace(',','').replace('.','').replace(':','').lower().split()
@@ -61,10 +69,11 @@ def pythonica(text):
             lista.append(p)
     return lista
 
-print(pythonica(text))
+print(f'Há {len(pythonica(text))} palavras que possuem uma das letras de "python".\n')
+input('\nAperte Enter para prosseguir...\n')
 
-print("        ===EXERCICIO_5===\n")
-
+print('        ===EXERCICIO_5===\n')
+print('Usando o mesmo texto do exercicio 4 utilize-se da função split() e  calcule quantas palavras com mais de 4 letras possuem alguma letra das letras de "python".\n')
 text = 'The Python Software Foundation and the global Python community welcome and encourage participation by everyone. Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles. We want our community to be more diverse: whoever you are, and whatever your background, we welcome you'
 text = text.replace(',','').replace('.','').replace(':','').lower().split()
 lista = []
@@ -77,4 +86,4 @@ def pythonicus(text):
                     lista.append(p)
                     break
     return lista
-print(pythonicus(text))
+print(f'Há {len(pythonicus(text))} palavras com mais de 4 letras que possuem uma das letras de "python".\n')
